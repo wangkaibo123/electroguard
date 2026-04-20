ca7137b39b4934ffb2b1f9264f69ca14a082a11a
wangkaiboCN <112166533+wangkaiboCN@users.noreply.github.com>
2026-04-20 04:32:34 +0000

    更新配置与构建产物

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: { '@': path.resolve(__dirname, '.') },
